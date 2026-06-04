002eb7158df2bc3f13de28ee0dac8ab3920225e3
Anton Northern <48867587+koshikishi@users.noreply.github.com>
2026-06-04 22:58:36 +0300

    chore: update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,8 @@
 import {defineConfig} from 'eslint/config';
 import config from '@koshikishi/eslint-config/node';
-import globals from 'globals';
 
 export default defineConfig([
   {
     extends: [config],
-    languageOptions: {
-      globals: globals.node,
-    },
   },
 ]);
